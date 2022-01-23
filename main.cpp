ecdf0cfc7cb98fefae31ddb157c727e78c239f33
Konstantin Kutsenko <ssidein@gmail.com>
2022-01-24 01:02:14 +0300

    Main cleaning

--- main.cpp
+++ main.cpp
@@ -5,214 +5,48 @@
 #include "PrimeMusic.hpp"
 #include "NoteOperations.hpp"
 
-const size_t primesCount = 28;
-int primeList [primesCount] = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109 };
+
+std::vector<int> primeList = { 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                               73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157,
+                               163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263,
+                               269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809,
+                               811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941,
+                               947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069,
+                               1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223,
+                               1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373,
+                               1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511,
+                               1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657,
+                               1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811,
+                               1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987,
+                               1993, 1997, 1999, 2003, 2011, 2017, 2027, 2029, 2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111, 2113, 2129,
+                               2131, 2137, 2141, 2143, 2153, 2161, 2179, 2203, 2207, 2213, 2221, 2237, 2239, 2243, 2251, 2267, 2269, 2273, 2281, 2287,
+                               2293, 2297, 2309, 2311, 2333, 2339, 2341, 2347, 2351, 2357, 2371, 2377, 2381, 2383, 2389, 2393, 2399, 2411, 2417, 2423,
+                               2437, 2441, 2447, 2459, 2467, 2473, 2477, 2503, 2521, 2531, 2539, 2543, 2549, 2551, 2557, 2579, 2591, 2593, 2609, 2617,
+                               2621, 2633, 2647, 2657, 2659, 2663, 2671, 2677, 2683, 2687, 2689, 2693, 2699, 2707, 2711, 2713, 2719, 2729, 2731, 2741,
+                               2749, 2753, 2767, 2777, 2789, 2791, 2797, 2801, 2803, 2819, 2833, 2837, 2843, 2851, 2857, 2861, 2879, 2887, 2897, 2903,
+                               2909, 2917, 2927, 2939, 2953, 2957, 2963, 2969, 2971, 2999, 3001, 3011, 3019, 3023, 3037, 3041, 3049, 3061, 3067, 3079,
+                               3083, 3089, 3109, 3119, 3121, 3137, 3163, 3167, 3169, 3181, 3187, 3191, 3203, 3209, 3217, 3221, 3229, 3251, 3253, 3257,
+                               3259, 3271, 3299, 3301, 3307, 3313, 3319, 3323, 3329, 3331, 3343, 3347, 3359, 3361, 3371, 3373, 3389, 3391, 3407, 3413,
+                               3433, 3449, 3457, 3461, 3463, 3467, 3469, 3491, 3499, 3511, 3517, 3527, 3529, 3533, 3539, 3541, 3547, 3557, 3559, 3571 };
 
 
+void printPrimeIntervals(const std::vector<int>& sequence) {
+    for (size_t i = 0; i < sequence.size(); ++i) {
+        const long double semitones = 12 * std::log2(
+                    static_cast<long double>(sequence[i]));
+        const uint32_t closestSemitones = std::round(semitones);
+        const long double cents = (semitones - closestSemitones) * 100.0;
 
-std::vector<int> exploreCyclesAndIntervals(int* sequence, size_t len,
-                                           int mult = 1, bool log = false)
-{
-    PrimeMusic p;
-    std::vector<int> intervals;
-    for (size_t i = 0; i < len; ++i) {
-        p.setup(sequence[i] * mult);
-        p.findPerfectCycle(log);
-        const auto [interval, _] = p.musicInterval();
-        intervals.push_back(interval);
-    }
-    return intervals;
-}
-
-
-std::vector<double> exploreCents(int* sequence, size_t len,
-                                    int mult = 1)
-{
-    PrimeMusic p;
-    std::vector<double> intervals;
-    for (size_t i = 0; i < len; ++i) {
-        p.setup(sequence[i] * mult);
-        const auto [_, cents] = p.musicInterval();
-        intervals.push_back(cents);
-    }
-    return intervals;
-}
-
-
-std::vector<std::pair<int, double>> exploreWhenCycleBreaks(int* sequence,
-                                    size_t len, int mult = 1, bool log = false)
-{
-    PrimeMusic p;
-    std::vector<std::pair<int, double>> breakPoints;
-
-    for (size_t i = 0; i < len; ++i) {
-        p.setup(sequence[i] * mult);
-        breakPoints.push_back(p.whenCycleBreaks(log));
-    }
-
-    return breakPoints;
-}
-
-
-std::vector<std::pair<int, double>> exploreWhenCycleConverges(int* sequence, size_t len, int mult = 1, bool log = false)
-{
-    PrimeMusic p;
-    std::vector<std::pair<int, double>> convPoints;
-
-    for (size_t i = 0; i < len; ++i) {
-        p.setup(sequence[i] * mult);
-        convPoints.push_back(p.whenCycleConverges(1., log));
-    }
-
-    return convPoints;
-}
-
-//TODO new class PrimeMusicSequence - через init list - так же находить циклы итд
-
-
-std::vector<int> noteDiffVector(const std::vector<int>& lhs,
-                const std::vector<int>& rhs, bool noteNorm = true)
-{
-    if (lhs.size() != rhs.size())
-        return {};
-
-    std::vector<int> diffs;
-
-    for (size_t i = 0; i < lhs.size(); ++i) {
-        int diff = (lhs[i] % 12) - (rhs[i] % 12);
-        if (noteNorm && diff < 0)
-            diff += 12;
-         diffs.push_back(diff);
-    }
-
-    return diffs;
-}
-
-
-std::vector<int> mostFrequentValues(const std::vector<int>& v) {
-    std::unordered_map<int, size_t> entries;
-
-    for (auto element: v) {
-        if (entries.count(element))
-            entries[element] += 1;
-        else
-            entries[element] = 1;
-    }
-
-    std::vector<std::pair<int,size_t>> sorted(entries.begin(), entries.end());
-    std::sort(sorted.begin(), sorted.end(), [](auto& lhs, auto& rhs){ return lhs.second > rhs.second;});
-
-    std::vector<int> mostFreq;
-    for (auto s: sorted)
-        mostFreq.push_back(s.first);
-
-    return mostFreq;
-}
-
-
-void explorePrimeProducts() {
-    auto basicIntervals = exploreCyclesAndIntervals(primeList, primesCount);
-
-    for (size_t i = 0; i < primesCount; ++i)
-    {
-        const int prime = primeList[i];
-        auto nextIntervals = exploreCyclesAndIntervals(primeList, primesCount, prime);
-        auto diffVector = noteDiffVector(nextIntervals, basicIntervals);
-        auto mostFreq = mostFrequentValues(diffVector);
-
-        int mostDiff = mostFreq[1] - mostFreq[0];
-        if (mostDiff == -11)
-            mostDiff = 1;
-
-        std::string expectationString;
-
-        if ((basicIntervals[i] % 12) == mostFreq[0])
-            expectationString = " Most frequent equal";
-        else if ((basicIntervals[i] % 12) == mostFreq[1])
-            expectationString = " Second frequent equal";
-        else
-            expectationString = " Something unexpected";
-
-        std::cout << prime << ") " << mostFreq[0] << " Total " << mostFreq.size() <<
-                  " Diff " << mostDiff << expectationString << std::endl;
+        std::cout << sequence[i] << ", " << closestSemitones << ", "
+                  << closestSemitones % 12 << ", " << cents << std::endl;
     }
 }
-
-
-void exploreMultiProducts() {
-    auto basicIntervals = exploreCyclesAndIntervals(primeList, primesCount);
-
-    const size_t multSize = 6; //3
-    int multProducts[multSize] = {15, 21, 33, 35, 39, 55};
-    //int multProducts[multSize] = {9, 25, 49};
-
-    for (size_t i = 0; i < multSize; ++i)
-    {
-        const int number = multProducts[i];
-        auto nextIntervals = exploreCyclesAndIntervals(primeList, primesCount, number);
-        auto diffVector = noteDiffVector(nextIntervals, basicIntervals);
-        auto mostFreq = mostFrequentValues(diffVector);
-
-        std::cout << number << ") " << mostFreq[0] << " " << mostFreq[1] << std::endl;
-    }
-}
-
-
-void exploreCycleBreakOnMult() {
-    auto usual = exploreWhenCycleBreaks(primeList, primesCount, 1);
-    auto mult = exploreWhenCycleBreaks(primeList, primesCount, 3);
-
-    for (size_t i = 0; i < usual.size(); ++i) {
-        std::cout << primeList[i] << ") " << usual[i].first << " "
-                  << usual[i].second << std::endl;
-        std::cout << primeList[i] * 3 << ") " << mult[i].first << " "
-                  << mult[i].second << std::endl;
-
-        std::cout << std::endl;
-    }
-}
-
-void exploreCycleCoverageOnMult() {
-    auto usual = exploreWhenCycleConverges(primeList, primesCount, 1);
-    auto mult = exploreWhenCycleConverges(primeList, primesCount, 3);
-
-    for (size_t i = 0; i < usual.size(); ++i) {
-        std::cout << primeList[i] << ") " << usual[i].first << " "
-                  << usual[i].second << std::endl;
-        std::cout << primeList[i] * 3 << ") " << mult[i].first << " "
-                  << mult[i].second << std::endl;
-
-        std::cout << std::endl;
-    }
-}
-
-
-void exploreCents(const int prime = 3) {
-
-    auto usual = exploreCents(primeList, primesCount, 1);
-    auto mult = exploreCents(primeList, primesCount, prime);
-    auto multNext = exploreCents(primeList, primesCount, prime * prime);
-
-    for (size_t i = 0; i < usual.size(); ++i) {
-        std::cout << primeList[i] << ") " << usual[i] << std::endl;
-        std::cout << primeList[i] * prime << ") " << mult[i] << " _ "
-                     << mult[i]  - usual[i] << std::endl;
-        std::cout << primeList[i] * prime * prime << ") " << multNext[i] << " _ "
-                  << multNext[i]  - usual[i] <<  " _ " << multNext[i]  - mult[i] << std::endl;
-        std::cout << std::endl;
-    }
-}
-
 
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) ///TODO эта функия будет просто как пример, офрмить в виде библиотеки
 {
 
-    //exploreCycleBreakOnMult(); // TODO unfinished - no conculsion was done
-    //exploreCycleCoverageOnMult(); // TODO unfinished - no conculsion was done
-    //exploreCents(2);
-
-    exploreCyclesAndIntervals(primeList, primesCount, 1, true);
+    printPrimeIntervals(primeList);
 
     return 0;
 }
