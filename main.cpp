560051ccd5076fb1af41f88fbec5b2ffe3717a99
Konstantin Kutsenko <ssidein@gmail.com>
2022-01-23 19:30:19 +0300

    Multiply exploring

--- main.cpp
+++ main.cpp
@@ -1,40 +1,25 @@
 #include <iostream>
-#include <set>
+#include <unordered_map>
 
 #include "PrimeMusic.hpp"
 #include "NoteOperations.hpp"
 
 const size_t primesCount = 28;
-int primeList [primesCount] = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
-                    59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109 };
+int primeList [primesCount] = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109 };
 
 
 
-void exploreCyclesAndIntervals(int* sequence, size_t len, int mult = 1)
+std::vector<int> exploreCyclesAndIntervals(int* sequence, size_t len, int mult = 1, bool log = false)
 {
     PrimeMusic p;
-    std::set<int> intervalsSet;
-
-    //Just a reminded 2 generated all perfect harmonics
-    //3 and 5 generated 1-3-5 major intervals
-
+    std::vector<int> intervals;
     for (size_t i = 0; i < len; ++i) {
         p.setup(sequence[i] * mult);
-        p.findPerfectCycle(true);
-
+        p.findPerfectCycle(log);
         const auto [interval, _] = p.musicInterval();
-        intervalsSet.insert(interval % 12);
-
-        if (intervalsSet.size() == 11) {
-            std::cout << "We got full set!" << std::endl;
-
-            for (auto i: intervalsSet)
-                std::cout << i << " ";
-            std::cout << std::endl;
-
-            break;
-        }
+        intervals.push_back(interval);
     }
+    return intervals;
 }
 
 
@@ -70,19 +55,55 @@ void exploreWhenCycleConverges(int* sequence, size_t len, int mult = 1)
 //TODO new class PrimeMusicSequence - через init list - так же находить циклы итд
 
 
+std::vector<int> noteDiffVector(const std::vector<int>& lhs,
+                const std::vector<int>& rhs, bool noteNorm = true)
+{
+    if (lhs.size() != rhs.size())
+        return {};
+
+    std::vector<int> diffs;
+
+    for (size_t i = 0; i < lhs.size(); ++i) {
+        int diff = (lhs[i] % 12) - (rhs[i] % 12);
+        if (noteNorm && diff < 0)
+            diff += 12;
+         diffs.push_back(diff);
+    }
+
+    return diffs;
+}
+
+
+std::vector<int> mostFrequentValues(const std::vector<int>& v) {
+    return {};
+}
+
+
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
-    exploreCyclesAndIntervals(primeList, primesCount);
+    //exploreCyclesAndIntervals(primeList, primesCount);
     //exploreWhenCycleBreaks(primeList, primesCount);
     //exploreWhenCycleConverges(primeList, primesCount);
 
-    std::cout << std::endl << "NOW *2 " << std::endl  << std::endl;
-
-    exploreCyclesAndIntervals(primeList, primesCount, 5);
+    //exploreCyclesAndIntervals(primeList, primesCount, 5);
     //exploreWhenCycleBreaks(primeList, primesCount, 2);
     //exploreWhenCycleConverges(primeList, primesCount, 2);
 
+    auto basicIntervals = exploreCyclesAndIntervals(primeList, primesCount);
+
+    for (size_t i = 0; i < primesCount; ++i) {
+
+        const int prime = primeList[i];
+
+        auto nextIntervals = exploreCyclesAndIntervals(primeList, primesCount, prime);
+        auto diffVector = noteDiffVector(nextIntervals, basicIntervals);
+
+        std::cout << "For " << prime << " diff vector: " << std::endl;
+        for (auto d: diffVector)
+            std::cout << d << " ";
+        std::cout << std::endl <<  std::endl;
+    }
 
     return 0;
 }
